26690864b6ec3e16f291500471f89f5661ec5e91
Ali Eren <devalierentabak@outlook.com>
2025-03-21 01:36:30 +0000

    security improvements

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["1000 per day", "200 per hour", "20 per minute", "3 per second"]
+    default_limits=["1000 per day", "200 per hour", "20 per minute", "10 per second"]
 )
 
 limiter.init_app(app)
